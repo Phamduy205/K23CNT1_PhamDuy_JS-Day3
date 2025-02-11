ee6b3896948467a99ef7023d632df5f9b2824868
Phamduy205 <duyqd.qd2005@gmail.com>
2025-02-11 15:15:32 +0700

    Day2

---
+++ projectJS-Day2/pvd-hello/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Thông tin cá nhân</h1>
+        <p>
+          <strong>Họ và tên:</strong> Phạm Văn Duy
+        </p>
+        <p>
+          <strong>Mã sinh viên:</strong> 2310900029
+        </p>
+        <p>
+          <strong>Ngày sinh:</strong> 02/12/2005
+        </p>
+        <p>
+          <strong>Điện thoại:</strong> 0123456789
+        </p>
+        <p>
+          <strong>Tên lớp:</strong> K23CNT1
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
